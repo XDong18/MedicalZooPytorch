17cd416d2fd5ecf4784258a2b07baa841bc9043e
XDong18 <liuxd1215@gmail.com>
2021-04-05 11:03:43 +0800

    change file path

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,4 @@
-python examples/train_pancreas.py --dataset_name pancreas \
+python train_pancreas.py --dataset_name pancreas \
 --batchSz 4 \
 --classes 3 \
 --path /shared/xudongliu/pancreas/Task07_Pancreas/dataset.json \
