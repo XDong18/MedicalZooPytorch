de1bf112ff3f4f8da9b0659b08f618412d5b54ee
XDong18 <liuxd1215@gmail.com>
2021-04-06 22:25:37 +0800

    add load

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,5 +3,6 @@ python train_pancreas.py --dataset_name pancreas \
 --classes 3 \
 --path /shared/xudongliu/pancreas/Task07_Pancreas \
 --anno_file /shared/xudongliu/pancreas/Task07_Pancreas/dataset.json \
+--loadData \
 --samples_train 5 \
 --samples_val 2
