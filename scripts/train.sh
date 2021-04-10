f1143f7eb1dff4c9b23e789094fb29b67972b79b
XDong18 <liuxd1215@gmail.com>
2021-04-10 18:33:06 +0800

    add load

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,9 +1,8 @@
 python train_pancreas.py --dataset_name pancreas \
 --batchSz 1 \
 --classes 3 \
---path /shared/xudongliu/pancreas/Task07_Pancreas \
---anno_file /shared/xudongliu/pancreas/Task07_Pancreas/dataset.json \
---loadData \
---samples_train 5 \
---samples_val 2 \
---nEpochs 5
+--path /home/bell-chuangxingang/xudong/Task07_Pancreas \
+--anno_file /home/bell-chuangxingang/xudong/Task07_Pancreas/dataset.json \
+--samples_train 500 \
+--samples_val 100 \
+--nEpochs 1
